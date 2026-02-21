e793104380ff26fcaea429bd39ffa10f2850518f
Anand Singh <pb11aps97@gmail.com>
2026-02-21 11:39:55 -0500

    Initial commit

---
+++ src/main/java/com/anad/excel_orchestrator/ExcelOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.anad.excel_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anad/excel_orchestrator/ExcelOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anad.excel_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
